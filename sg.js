716d8d8afa1cdf011ef8d37fa1d7bcea41c8c5ba
Anton Ilyushenkov <ilyushenkov@corp.mail.ru>
2012-12-03 16:05:16 +0300

    - sg.yass module - some up

--- sg.js
+++ sg.js
@@ -1,282 +1,4 @@
-/**!
-* YASS 0.3.8 - The fastest CSS selectors JavaScript library
-* JSX 1.1 - Multi-events and components loading library
-*
-* Copyright (c) 2008-2009 Nikolay Matsievsky aka sunnybear (webo.in),
-* 2007 Andrew Sumin (jsx.ru)
-* Dual licensed under the MIT (MIT-LICENSE.txt)
-* and GPL (GPL-LICENSE.txt) licenses.
-*
-* $Date: 2009-05-04 12:26:33 +3000 (Mon, 04 May 2009) $
-* $Rev: 371 $
-*/
-(function( window, document ){
-
-/**
- * Приводит like-array объект к Array типу
- * @param {Array-like object} arrayLike
- * @return {Array}
- */
-function makeArray( arrayLike ) {
-	var	arr,
-		l;
-	if( arrayLike && ( l = arrayLike.length ) !== undefined ) {
-		try {
-			arr = Array.prototype.slice.call( arrayLike );
-		} catch(e) {
-			arr = new Array( l );
-			while( l-- ) {
-				arr[ l ] = arrayLike[ l ];
-			}
-		}
-	}
-	return arr || [];
-}
-
-/* cached check for getElementsByClassName */
-var k = !!document.getElementsByClassName,
-
-/* cached check for querySelectorAll. Disable all IE due to lask of support */
-	q = !!document.querySelectorAll;
-
-
-/**
- * Returns number of nodes or an empty array
- * @param {String} CSS selector
- * @param {DOM node} root to look into
- */
-var _ = function (selector, root ) {
-	/* sets of nodes, to handle comma-separated selectors */
-	var sets = [];
-	
-	/* clean root with document */
-	root = root ? _( root )[0] : document;
-	
-	if( !selector ) {
-		return sets;
-	}
-	
-	if(
-		selector.nodeType || typeof selector === "object" && "setTimeout" in selector
-	)
-	{
-		return [ selector ];
-	}
-	
-	if(
-		typeof selector === "object" &&
-		( Object.prototype.toString.call( selector ) === "[object Array]" || selector.length !== undefined )
-	)
-	{
-		return makeArray( selector );
-	}
-	
-	if( !root ) {
-		return sets;
-	}
-	
-/* quick return or generic call, missed ~ in attributes selector */
-	if( /^[\w#.][\w\]*^|=!]*$/.test( selector ) ) {
-/*
-some simple cases - only ID or only CLASS for the very first occurence
-- don't need additional checks. Switch works as a hash.
-*/
-		var idx = 0;
-/* the only call -- no cache, thx to GreLI */
-		switch (selector.charAt(0)) {
-			case '#':
-				idx = selector.slice(1);
-				sets = document.getElementById(idx);
-/*
-workaround with IE bug about returning element by name not by ID.
-Solution completely changed, thx to deerua.
-Get all matching elements with this id
-*/
-				if ( sets && sets.id !== idx) {
-					sets = document.all[idx];
-				}
-				sets = sets ? [sets] : [];
-				break;
-			case '.':
-				var klass = selector.slice(1);
-				if ( k ) {
-					sets = (idx = (sets = root.getElementsByClassName(klass)).length) ? sets : [];
-				} else {
-/* no RegExp, thx to DenVdmj */
-					klass = ' ' + klass + ' ';
-					var nodes = root.getElementsByTagName('*'),
-						i = 0,
-						node;
-					while (node = nodes[i++]) {
-						if ((' ' + node.className + ' ').indexOf(klass) != -1) {
-							sets[idx++] = node;
-						}
-
-					}
-					sets = idx ? sets : [];
-				}
-				break;
-			default:
-				sets = (idx = (sets = root.getElementsByTagName(selector)).length) ? sets : [];
-				break;
-		}
-	} else {
-		
-		var failQuerySelectorAll = false,
-			selectorFindNorEqual = ~selector.indexOf('!=');
-/*
-all other cases. Apply querySelector if exists.
-All methods are called via . not [] - thx to arty
-*/
-		if( q && !selectorFindNorEqual ) {
-/* replace not quoted args with quoted one -- Safari doesn't understand either */
-			try {
-				sets = root.querySelectorAll( selector.replace( /=([^\]]+)/, '="$1"' ) );
-			} catch( e ) {
-				failQuerySelectorAll = true;
-			}
-/* generic function for complicated selectors */
-		}
-		
-		if( failQuerySelectorAll || !q || selectorFindNorEqual ) {
-/* number of groups to merge or not result arrays */
-/*
-groups of selectors separated by commas.
-Split by RegExp, thx to tenshi.
-*/
-			var groups = selector.split(/ *, */),
-/* group counter */
-				gl = groups.length - 1,
-/* if we need to concat several groups */
-				concat = !!gl,
-				group,
-				singles,
-				singles_length,
-/* to handle RegExp for single selector */
-				single,
-				i,
-/* to remember ancestor call for next childs, default is " " */
-				ancestor,
-/* current set of nodes - to handle single selectors */
-				nodes,
-/* for inner looping */
-				tag, id, klass, attr, eql, mod, ind, newNodes, idx, J, child, last, childs, item, h;
-/* loop in groups, maybe the fastest way */
-			while (group = groups[gl--]) {
-/*
-Split selectors by space - to form single group tag-id-class,
-or to get heredity operator. Replace + in child modificators
-to % to avoid collisions. Additional replace is required for IE.
-Replace ~ in attributes to & to avoid collisions.
-*/	
-				singles_length = (singles = group.replace(/(\([^)]*)\+/,"$1%").replace(/(\[[^\]]+)~/,"$1&").replace(/(~|>|\+)/," $1 ").split(/ +/)).length;
-				i = 0;
-				ancestor = ' ';
-/* is cleanded up with DOM root */
-				nodes = [root];
-/*
-John's Resig fast replace works a bit slower than
-simple exec. Thx to GreLI for 'greed' RegExp
-*/
-				while (single = singles[i++]) {
-/* simple comparison is faster than hash */
-					if (single !== ' ' && single !== '>' && single !== '~' && single !== '+' && nodes) {
-						single = single.match(/([^[:.#]+)?(?:#([^[:.#]+))?(?:\.([^[:.]+))?(?:\[([^!&^*|$[:=]+)([!$^*|&]?=)?([^:\]]+)?\])?(?:\:([^(]+)(?:\(([^)]+)\))?)?/);
-/* 
-Get all required matches from exec:
-tag, id, class, attribute, value, modificator, index.
-*/
-						tag = single[1] || '*';
-						id = single[2];
-						klass = single[3] ? ' ' + single[3] + ' ' : '';
-/* new nodes array */
-						newNodes = [];
-/* 
-cached length of new nodes array
-and length of root nodes
-*/
-						idx = J = 0;
-/* if we need to mark node with expando yeasss */
-						last = i == singles_length;
-/* loop in all root nodes */
-						while (child = nodes[J++]) {
-/*
-find all TAGs or just return all possible neibours.
-Find correct 'children' for given node. They can be
-direct childs, neighbours or something else.
-*/
-							switch (ancestor) {
-								case ' ':
-									childs = child.getElementsByTagName(tag);
-									h = 0;
-									while (item = childs[h++]) {
-/*
-check them for ID or Class. Also check for expando 'yeasss'
-to filter non-selected elements. Typeof 'string' not added -
-if we get element with name="id" it won't be equal to given ID string.
-Also check for given attributes selector.
-Modificator is either not set in the selector, or just has been nulled
-by modificator functions hash.
-*/
-										if ((!id || item.id === id) && (!klass || (' ' + item.className + ' ').indexOf(klass) != -1) && !item.yeasss) {
-/* 
-Need to define expando property to true for the last step.
-Then mark selected element with expando
-*/
-											if (last) {
-												item.yeasss = 1;
-											}
-											newNodes[idx++] = item;
-										}
-									}
-									break;
-							}
-						}
-/* put selected nodes in local nodes' set */
-						nodes = newNodes;
-					} else {
-/* switch ancestor ( , > , ~ , +) */
-						ancestor = single;
-					}
-				}
-				
-				if (concat) {
-/* if sets isn't an array - create new one */
-					if (!nodes.concat) {
-						newNodes = [];
-						h = 0;
-						while (item = nodes[h]) {
-							newNodes[h++] = item;
-						}
-						nodes = newNodes;
-/* concat is faster than simple looping */
-					}
-					sets = nodes.concat(sets.length == 1 ? sets[0] : sets);
-				} else {
-/* inialize sets with nodes */
-					sets = nodes;
-				}
-			}
-			
-/* define sets length to clean up expando */
-			idx = sets.length;
-/*
-Need this looping as far as we also have expando 'yeasss'
-that must be nulled. Need this only to generic case
-*/
-			while (idx--) {
-				sets[idx].yeasss = sets[idx].nodeIndex = sets[idx].nodeIndexLast = null;
-			}
-		}
-	}
-/* return and cache results */
-	return makeArray( sets );
-};
-
-window.yassmod = _;
-
-})( window, document );
-(function( window, yass, undefined ) {
+(function( window, undefined ) {
 	
 var	
 	document = window.document,
@@ -783,20 +505,6 @@ var SGUtils = {
 		return ret.join( "" );
 	},
 	
-	
-	/**
-	 * Получает один DOMNode по селектору
-	 * @param {String|DOMNode} mixed
-	 * @param {String|DOMNode} context [optional]
-	 * @return {DOMNode|Null}
-	 */
-	$: function( mixed, context ) {
-		return yass( mixed, context )[ 0 ] || null;
-	},
-	
-	
-	$$: yass,
-	
 	/**
 	 * Устанавливает/Получает css-свойство элемента.
 	 * 	Если 2-й агрумент не указан, то будет возвращены все css-свойства элемента
@@ -1064,7 +772,7 @@ SGUtils.tmpl = (function(){
 				res = cache[ selector ];
 			}
 			if( !res ) {
-				var elem = yass( selector )[ 0 ];
+				var elem = SGUtils.$( selector );
 				if( elem ) {
 					res = cache[ ( elem[ EXPANDO ] || ( elem[ EXPANDO ] = getGUID() ) ) ] = tmpl( elem.innerHTML || "" );
 					if( isString ) {
@@ -1109,9 +817,7 @@ SGUtils.tmpl = (function(){
 
 // Shorthands
 var extend = SGUtils.ext,
-	isFunction = SGUtils.isFn,
-	$ = SGUtils.$,
-	$$ = yass;
+	isFunction = SGUtils.isFn;
 
 
 // Event object part
@@ -2643,17 +2349,17 @@ function Suggest( inputOptions ) {
 	
 	var 
 		// Получаем инпут, к которому и подцепиться саджест
-		$field = options.field && $( options.field ),
+		$field = options.field && SGUtils.$( options.field ),
 		
 		// Форма, в которой находится инпут
 		$form = $field && $field.form,
 		
 		// Контейнер, который будет показываться при получении саджестов
-		$container = $( options.cont ),
+		$container = SGUtils.$( options.cont ),
 		
 		// Контейнер, к который будет строиться список подсказок
 		$list = options.list && $container
-			? $( options.list, $container )
+			? SGUtils.$( options.list, $container )
 			: $container,
 		
 		// Элементы подсказок
@@ -2819,8 +2525,274 @@ Suggest.utils = SGUtils;
 window.SG = window.SG || Suggest;
 
 
-})( window, window.yassmod );
+})( window );
 
+/**!
+* YASS 0.3.8 - The fastest CSS selectors JavaScript library
+* JSX 1.1 - Multi-events and components loading library
+*
+* Copyright (c) 2008-2009 Nikolay Matsievsky aka sunnybear (webo.in),
+* 2007 Andrew Sumin (jsx.ru)
+* Dual licensed under the MIT (MIT-LICENSE.txt)
+* and GPL (GPL-LICENSE.txt) licenses.
+*
+* $Date: 2009-05-04 12:26:33 +3000 (Mon, 04 May 2009) $
+* $Rev: 371 $
+*/
+(function( window, sg ){
+
+var document = window.document,
+	utils = sg.utils,
+	makeArray = utils.mkarr;
+
+/* cached check for getElementsByClassName */
+var k = !!document.getElementsByClassName,
+
+/* cached check for querySelectorAll. Disable all IE due to lask of support */
+	q = !!document.querySelectorAll;
+
+
+/**
+ * Returns number of nodes or an empty array
+ * @param {String} CSS selector
+ * @param {DOM node} root to look into
+ */
+var _ = function (selector, root ) {
+	/* sets of nodes, to handle comma-separated selectors */
+	var sets = [];
+	
+	/* clean root with document */
+	root = root ? _( root )[0] : document;
+	
+	if( !selector ) {
+		return sets;
+	}
+	
+	if(
+		selector.nodeType || typeof selector === "object" && "setTimeout" in selector
+	)
+	{
+		return [ selector ];
+	}
+	
+	if(
+		typeof selector === "object" &&
+		( Object.prototype.toString.call( selector ) === "[object Array]" || selector.length !== undefined )
+	)
+	{
+		return makeArray( selector );
+	}
+	
+	if( !root ) {
+		return sets;
+	}
+	
+/* quick return or generic call, missed ~ in attributes selector */
+	if( /^[\w#.][\w\]*^|=!]*$/.test( selector ) ) {
+/*
+some simple cases - only ID or only CLASS for the very first occurence
+- don't need additional checks. Switch works as a hash.
+*/
+		var idx = 0;
+/* the only call -- no cache, thx to GreLI */
+		switch (selector.charAt(0)) {
+			case '#':
+				idx = selector.slice(1);
+				sets = document.getElementById(idx);
+/*
+workaround with IE bug about returning element by name not by ID.
+Solution completely changed, thx to deerua.
+Get all matching elements with this id
+*/
+				if ( sets && sets.id !== idx) {
+					sets = document.all[idx];
+				}
+				sets = sets ? [sets] : [];
+				break;
+			case '.':
+				var klass = selector.slice(1);
+				if ( k ) {
+					sets = (idx = (sets = root.getElementsByClassName(klass)).length) ? sets : [];
+				} else {
+/* no RegExp, thx to DenVdmj */
+					klass = ' ' + klass + ' ';
+					var nodes = root.getElementsByTagName('*'),
+						i = 0,
+						node;
+					while (node = nodes[i++]) {
+						if ((' ' + node.className + ' ').indexOf(klass) != -1) {
+							sets[idx++] = node;
+						}
+
+					}
+					sets = idx ? sets : [];
+				}
+				break;
+			default:
+				sets = (idx = (sets = root.getElementsByTagName(selector)).length) ? sets : [];
+				break;
+		}
+	} else {
+		
+		var failQuerySelectorAll = false,
+			selectorFindNorEqual = ~selector.indexOf('!=');
+/*
+all other cases. Apply querySelector if exists.
+All methods are called via . not [] - thx to arty
+*/
+		if( q && !selectorFindNorEqual ) {
+/* replace not quoted args with quoted one -- Safari doesn't understand either */
+			try {
+				sets = root.querySelectorAll( selector.replace( /=([^\]]+)/, '="$1"' ) );
+			} catch( e ) {
+				failQuerySelectorAll = true;
+			}
+/* generic function for complicated selectors */
+		}
+		
+		if( failQuerySelectorAll || !q || selectorFindNorEqual ) {
+/* number of groups to merge or not result arrays */
+/*
+groups of selectors separated by commas.
+Split by RegExp, thx to tenshi.
+*/
+			var groups = selector.split(/ *, */),
+/* group counter */
+				gl = groups.length - 1,
+/* if we need to concat several groups */
+				concat = !!gl,
+				group,
+				singles,
+				singles_length,
+/* to handle RegExp for single selector */
+				single,
+				i,
+/* to remember ancestor call for next childs, default is " " */
+				ancestor,
+/* current set of nodes - to handle single selectors */
+				nodes,
+/* for inner looping */
+				tag, id, klass, attr, eql, mod, ind, newNodes, idx, J, child, last, childs, item, h;
+/* loop in groups, maybe the fastest way */
+			while (group = groups[gl--]) {
+/*
+Split selectors by space - to form single group tag-id-class,
+or to get heredity operator. Replace + in child modificators
+to % to avoid collisions. Additional replace is required for IE.
+Replace ~ in attributes to & to avoid collisions.
+*/	
+				singles_length = (singles = group.replace(/(\([^)]*)\+/,"$1%").replace(/(\[[^\]]+)~/,"$1&").replace(/(~|>|\+)/," $1 ").split(/ +/)).length;
+				i = 0;
+				ancestor = ' ';
+/* is cleanded up with DOM root */
+				nodes = [root];
+/*
+John's Resig fast replace works a bit slower than
+simple exec. Thx to GreLI for 'greed' RegExp
+*/
+				while (single = singles[i++]) {
+/* simple comparison is faster than hash */
+					if (single !== ' ' && single !== '>' && single !== '~' && single !== '+' && nodes) {
+						single = single.match(/([^[:.#]+)?(?:#([^[:.#]+))?(?:\.([^[:.]+))?(?:\[([^!&^*|$[:=]+)([!$^*|&]?=)?([^:\]]+)?\])?(?:\:([^(]+)(?:\(([^)]+)\))?)?/);
+/* 
+Get all required matches from exec:
+tag, id, class, attribute, value, modificator, index.
+*/
+						tag = single[1] || '*';
+						id = single[2];
+						klass = single[3] ? ' ' + single[3] + ' ' : '';
+/* new nodes array */
+						newNodes = [];
+/* 
+cached length of new nodes array
+and length of root nodes
+*/
+						idx = J = 0;
+/* if we need to mark node with expando yeasss */
+						last = i == singles_length;
+/* loop in all root nodes */
+						while (child = nodes[J++]) {
+/*
+find all TAGs or just return all possible neibours.
+Find correct 'children' for given node. They can be
+direct childs, neighbours or something else.
+*/
+							switch (ancestor) {
+								case ' ':
+									childs = child.getElementsByTagName(tag);
+									h = 0;
+									while (item = childs[h++]) {
+/*
+check them for ID or Class. Also check for expando 'yeasss'
+to filter non-selected elements. Typeof 'string' not added -
+if we get element with name="id" it won't be equal to given ID string.
+Also check for given attributes selector.
+Modificator is either not set in the selector, or just has been nulled
+by modificator functions hash.
+*/
+										if ((!id || item.id === id) && (!klass || (' ' + item.className + ' ').indexOf(klass) != -1) && !item.yeasss) {
+/* 
+Need to define expando property to true for the last step.
+Then mark selected element with expando
+*/
+											if (last) {
+												item.yeasss = 1;
+											}
+											newNodes[idx++] = item;
+										}
+									}
+									break;
+							}
+						}
+/* put selected nodes in local nodes' set */
+						nodes = newNodes;
+					} else {
+/* switch ancestor ( , > , ~ , +) */
+						ancestor = single;
+					}
+				}
+				
+				if (concat) {
+/* if sets isn't an array - create new one */
+					if (!nodes.concat) {
+						newNodes = [];
+						h = 0;
+						while (item = nodes[h]) {
+							newNodes[h++] = item;
+						}
+						nodes = newNodes;
+/* concat is faster than simple looping */
+					}
+					sets = nodes.concat(sets.length == 1 ? sets[0] : sets);
+				} else {
+/* inialize sets with nodes */
+					sets = nodes;
+				}
+			}
+			
+/* define sets length to clean up expando */
+			idx = sets.length;
+/*
+Need this looping as far as we also have expando 'yeasss'
+that must be nulled. Need this only to generic case
+*/
+			while (idx--) {
+				sets[idx].yeasss = sets[idx].nodeIndex = sets[idx].nodeIndexLast = null;
+			}
+		}
+	}
+/* return and cache results */
+	return makeArray( sets );
+};
+
+
+// share
+sg.$ = utils.$ = function( mixed, context ) {
+	return _( mixed, context )[ 0 ] || null;
+};
+sg.$$ = utils.$$ = _;
+
+})( window, SG );
 (function( window, sg ) {
 
 var document = window.document,
