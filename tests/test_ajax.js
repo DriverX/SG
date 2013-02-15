e114a1b8a13735655d66127bcc5dffbdc8e59cd1
Anton Ilyushenkov <driverx_strogino@mail.ru>
2013-02-15 17:25:36 +0400

    up

--- tests/test_ajax.js
+++ tests/test_ajax.js
@@ -9,20 +9,19 @@ asyncTest("basic", function() {
 
   SG.Ajax("data/test_ajax_simple.html", {
     success: function( event, response ) {
-      alert( 1 );
       equal( response, "<!-- passed -->\n" );
       start(); 
     }
   }).send();
  
-  // stop();
-  // SG.Ajax("data/test_ajax_simple.html", {
-  //   method: "post",
-  //   success: function( event, response ) {
-  //     equal( response, "<!-- passed -->\n" );
-  //     start(); 
-  //   }
-  // }).send();
+  stop();
+  SG.Ajax("data/test_ajax_simple.html", {
+    method: "post",
+    success: function( event, response ) {
+      equal( response, "<!-- passed -->\n" );
+      start(); 
+    }
+  }).send();
 });
 
 
